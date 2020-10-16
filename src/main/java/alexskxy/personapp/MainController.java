9d5ef1f64c1ecba648103b8430a3f2f22c6b7e0f
alex-skxy <alex13skx@gmail.com>
2020-10-16 08:42:41 +0200

    add onAction and fx:ids plus controller skeleton

--- src/main/java/alexskxy/personapp/MainController.java
+++ src/main/java/alexskxy/personapp/MainController.java
@@ -1,11 +1,36 @@
 package alexskxy.personapp;
 
+import alexskxy.personapp.entity.Person;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.List;
 
 public class MainController {
+    private List<Person> people;
+    private List<TextField> toValidate;
+    @FXML
+    public TextField personalNummer;
+    @FXML
+    public TextField anrede;
+    @FXML
+    public TextField name;
+    @FXML
+    public TextField vorname;
+    @FXML
+    public TextField plz;
+    @FXML
+    public TextField ort;
+    @FXML
+    public TextField eintrittsJahr;
+    @FXML
+    public TextField salaer;
+    @FXML
+    public TextField pensum;
+
     public Button button;
     @FXML
     private Label label;
@@ -14,9 +39,39 @@ public class MainController {
         // TODO
     }
 
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        label.setText("Hello World!");
+    private void setViewObject(Person person) {
+        personalNummer.setText(person.getPersonalNummer().toString());
+        anrede.setText(person.getAnrede());
+        name.setText(person.getName());
+        vorname.setText(person.getVorname());
+        plz.setText(person.getPlz());
+        ort.setText(person.getOrt());
+        eintrittsJahr.setText(person.getEintrittsJahr().toString());
+        salaer.setText(person.getSalaer().toString());
+        pensum.setText(person.getPensum().toString());
     }
 
+    public void handleGetFirst(ActionEvent actionEvent) {
+    }
+
+    public void handleGetPrevious(ActionEvent actionEvent) {
+    }
+
+    public void handleGetNext(ActionEvent actionEvent) {
+    }
+
+    public void handleGetLast(ActionEvent actionEvent) {
+    }
+
+    public void handleAdd(ActionEvent actionEvent) {
+    }
+
+    public void handleRemove(ActionEvent actionEvent) {
+    }
+
+    public void handleCancel(ActionEvent actionEvent) {
+    }
+
+    public void handleSave(ActionEvent actionEvent) {
+    }
 }
