5be90e798316fdd7710a19a7b13fdc8a4a7904fb
alex-skxy <alex13skx@gmail.com>
2020-10-16 09:33:38 +0200

    navigation

--- src/main/java/alexskxy/personapp/MainController.java
+++ src/main/java/alexskxy/personapp/MainController.java
@@ -4,14 +4,27 @@ import alexskxy.personapp.entity.Person;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Border;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainController {
-    private List<Person> people;
-    private List<TextField> toValidate;
+    private final List<Person> people;
+    private Integer currentPerson;
+
+    @FXML
+    public Button first;
+    @FXML
+    public Button previous;
+    @FXML
+    public Button next;
+    @FXML
+    public Button last;
+
+    @FXML
+    public TextField current;
     @FXML
     public TextField personalNummer;
     @FXML
@@ -31,15 +44,21 @@ public class MainController {
     @FXML
     public TextField pensum;
 
-    public Button button;
-    @FXML
-    private Label label;
-
-    public void initialize() {
-        // TODO
+    public MainController() {
+        this.people = new ArrayList<>();
     }
 
-    private void setViewObject(Person person) {
+    public void initialize() {
+        people.addAll(List.of(new Person(), new Person(), new Person()));
+        setViewObject(0);
+    }
+
+    private void setViewObject(Integer index) {
+        currentPerson = index;
+        current.setText(String.valueOf(currentPerson + 1));
+
+        Person person = people.get(index);
+
         personalNummer.setText(person.getPersonalNummer().toString());
         anrede.setText(person.getAnrede());
         name.setText(person.getName());
@@ -52,18 +71,35 @@ public class MainController {
     }
 
     public void handleGetFirst(ActionEvent actionEvent) {
+        Integer index = 0;
+        if (indexIsValid(index)) {
+            setViewObject(0);
+        }
     }
 
     public void handleGetPrevious(ActionEvent actionEvent) {
+        Integer index = currentPerson - 1;
+        if (indexIsValid(index)) {
+            setViewObject(index);
+        }
     }
 
     public void handleGetNext(ActionEvent actionEvent) {
+        Integer index = currentPerson + 1;
+        if (indexIsValid(index)) {
+            setViewObject(index);
+        }
     }
 
     public void handleGetLast(ActionEvent actionEvent) {
+        Integer index = people.size() - 1;
+        if (indexIsValid(index)) {
+            setViewObject(index);
+        }
     }
 
     public void handleAdd(ActionEvent actionEvent) {
+
     }
 
     public void handleRemove(ActionEvent actionEvent) {
@@ -73,5 +109,13 @@ public class MainController {
     }
 
     public void handleSave(ActionEvent actionEvent) {
+    }
+
+    private boolean indexIsValid(Integer index) {
+        if (people.isEmpty()) {
+            return false;
+        } else if (index == -1) {
+            return false;
+        } else return index != people.size();
     }
 }
